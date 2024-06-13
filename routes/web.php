f0a129427c775e6abcdc5a1edf5ff20a4b7bcf27
BernoBelmonte <berbelmonte18@gmail.com>
2024-06-13 11:33:56 -0400

    "controllers and routes"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\InventoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+Route::get('/inventories', [InventoryController::class, 'index'])->name('inventories.index');
